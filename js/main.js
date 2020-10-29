71ae35ac71cf21ef94f732b24ac86f378b224920
James Haff <jameshaff@Jamess-MacBook-Pro.local>
2020-10-29 15:44:26 -0500

    added rpsLookup logic

--- js/main.js
+++ js/main.js
@@ -53,7 +53,10 @@ document.querySelector('button')
 init(); // initialize our state when the app loads in the browser
 
 function init(){
-
+  // these properties in our state
+  // should have similiar key names to
+  // our cached elements where our state values
+  // will be rendered on the dom, refer to scoreEls and choiceEls
   scores = {
     player: 0,
     computer: 0,
@@ -61,7 +64,7 @@ function init(){
   };
   
   choices = {
-    player: "r",
+    player: "r", // 'r', 'p', 's'
     computer: "r"
   }
 
@@ -72,8 +75,53 @@ function init(){
 
 function render(){
   console.log('render is firing')
+
+  // render our score values to the dom, //reference our state for our score values, 
+  // and our cached elemeents scoreEls for their location
+  for (let score in scores){
+    // scores[score] value
+    scoreEls[score].innerText = scores[score];
+  }
+
+  // render out the intial choice, you'll have to use your rpsLookup
+  // choicesEl.player.imgEl.setAttribute('src', rpsLookup[choices.player].imageUrl);
+  // choicesEl.computer.imgEl.setAttribute('src', rpsLookup[choices.computer].imageUrl);
+
+  // loop style
+  for (let choice in choices){
+    //
+    // console.log(choice) // things to log
+    // console.log(choices[choice]) the value
+    choicesEl[choice].imgEl.src = rpsLookup[choices[choice]].imageUrl
+  }
 }
 
 function playRound(){
   console.log('play round is firing on the click of my button')
+
+  // determine choice 
+  // know whether the computer or user through paper scissors or rock
+  choices.player = getRandomRPS(); // what should this return 
+  choices.computer =  getRandomRPS(); // 'r', 'p', 's' refering to our choices object defined in our state
+  // go in to them dom after you press your button, then check your state
+  // What piece state are you updating?
+
+
+  // determine the winner
+
+  // update state 
+  // update our score
+
+  // render
+  render();
+}
+
+function getRandomRPS(){
+  // try to finish this function
+  const letters = ['r', 'p', 's'];
+  // another way 
+  // const letters = Object.keys(rpsLookup); // ['r', 'p', 's']
+  const rndIndex = Math.floor(Math.random() * 3);
+
+  return letters[rndIndex]
 }
